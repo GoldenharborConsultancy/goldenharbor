21cf43cc1ebb7297f3e5486d883e6e96ff4b0f08
Golden Harbor Consultancy <marcus.lim.ghb@gmail.com>
2026-06-08 14:24:24 +0800

    Add PWA app page for loan application submissions

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'goldenharbor-v1';
+const ASSETS = [
+  '/goldenharbor/app.html',
+  '/goldenharbor/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
